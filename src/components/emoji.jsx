cadedc26e684bfd2b65ffb86ff79be4605cc64f2
jachvictor <chukwujachike175@gmail.com>
2023-02-27 00:16:58 +0100

    first commit

---
+++ src/components/emoji.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import EmojiData from "../emoji.json";
+
+function Emoji() {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const newData = EmojiData.filter((emoji) =>
+      emoji.title.toLowerCase().includes(search.toLowerCase())
+    );
+    setData(newData);
+  }, [search]);
+  return (
+    <div>
+      <center>
+        <h1> 😎 Emoji Search</h1>
+        <input
+          className="input"
+          size="50"
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </center>
+      {data.map((emoji) => (
+        <div className="card" key={emoji.title}>
+          <div
+            className="card-body"
+            onClick={() => {
+              navigator.clipboard.writeText(emoji.symbol);
+              alert("Emoji Copy");
+            }}
+          >
+            {emoji.symbol} {emoji.title}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Emoji;
